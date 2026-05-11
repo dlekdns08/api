523a6068b40af640abb94febe61dde8f5619cab2
dlekdns08 <dlekdns08@gmail.com>
2026-05-11 18:44:37 +0900

    feat: 게임 라우터 추가

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from prometheus_fastapi_instrumentator import Instrumentator
 from sqlalchemy import text
 
 from database import engine, Base
-from routes import comments, likes, subscribe, reactions, views, knowledge_graph
+from routes import comments, likes, subscribe, reactions, views, knowledge_graph, game
 
 # 테이블 자동 생성
 Base.metadata.create_all(bind=engine)
@@ -41,6 +41,7 @@ app.include_router(reactions.router)
 app.include_router(views.router)
 app.include_router(subscribe.router)
 app.include_router(knowledge_graph.router)
+app.include_router(game.router)
 
 
 @app.get("/health")
